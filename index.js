f5c406454068cdb1d73dbd379f68c97dd3b8b621
ayaz-alijamali <ayazalijamali64@gmail.com>
2024-05-06 00:25:15 +0500

    100 days coding challenge Questions

---
+++ index.js
@@ -0,0 +1,18 @@
+// Question ______________ 118
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+;
+// Question __________________ 119
+let count = 0;
+while (count < 5) {
+    console.log("Hello, World!");
+    count++;
+}
+;
+// Question ________________ 120
+const favoriteMovies = ["fast and furious", "The Avengers", "Mission Unpossible", "Thor", "The Dark World", "UnderWorld"];
+for (const movie of favoriteMovies) {
+    console.log(movie);
+}
+export {};
